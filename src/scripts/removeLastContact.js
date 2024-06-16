74f7ec67eee088188eb1fe15d8b019e4cc3e3e17
Ihor Cherviakov <118768294+cherviakow@users.noreply.github.com>
2024-06-16 02:57:09 -0400

    update

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+    const deleteContact = contacts[contacts.length-1];
+    return deleteContact;
+
+};
 
 removeLastContact();
